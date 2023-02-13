84f3b6b453ee3828bad7164bdaae605bbf3c7c25
rinorramli <riramli2023@gmail.com>
2023-02-13 10:33:29 +0800

    before implementing supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:chatuiconcept/screen/welcome/welcome.dart';
 import 'package:chatuiconcept/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 //ui works on phones but needs adjusting to work on PC browsers.
-void main() {
+void main() async {
+  var url = 'http://localhost:8000';
+  var anonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE';
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: url,
+    anonKey: anonkey,
+  );
+  debugPrint("database connected");
   runApp(const MyApp());
   debugPrint("run completed");
 }
